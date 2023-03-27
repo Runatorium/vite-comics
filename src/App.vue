e762cbea1fb3f6d90f3ff31a24b8504462d30c33
Runatorium <stefano.alesci1@gmail.com>
2023-03-27 17:13:31 +0200

    Working on footer data Structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
+import TheFooter from './components/TheFooter.vue'
 import TheHeader from './components/TheHeader.vue'
+import MainContent from './components/MainContent.vue'
 </script>
 
 <template>
   <TheHeader></TheHeader>
+  <MainContent></MainContent>
+  <TheFooter></TheFooter>
 </template>
 
 <style>
