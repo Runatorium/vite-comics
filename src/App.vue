b6de75434e55dabde90d3d201f9ffd8893b6c80d
Runatorium <stefano.alesci1@gmail.com>
2023-03-28 15:17:27 +0200

    Working on blue bar (display dinamico immagini)

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import TheFooter from './components/TheFooter.vue'
 import TheHeader from './components/TheHeader.vue'
 import MainContent from './components/MainContent.vue'
 import BottomBar from './components/BottomBar.vue'
+import BlueBar from './components/BlueBar.vue'
 </script>
 
 <template>
   <TheHeader></TheHeader>
   <MainContent></MainContent>
+  <BlueBar></BlueBar>
   <TheFooter></TheFooter>
   <BottomBar></BottomBar>
 </template>
---
+++ src/components/BlueBar.vue
@@ -0,0 +1,68 @@
+<script>
+export default{
+    name: 'BlueBar',
+    data() {
+            return {
+                blueImgs: [
+                    {
+                        img: '../../img/buy-comics-digital-comics.png',
+                        text: 'Digital Comics'
+                    },
+                    {
+                        img: '../../img/buy-comics-merchandise.png',
+                        text: 'DC Merchandise'
+                    },
+                    {
+                        img: '../../img/buy-comics-subscriptions.png',
+                        text: 'Subscription'
+                    },
+                    {
+                        img: '../../img/buy-comics-shop-locator.png',
+                        text: 'Comic Shop Locator'
+                    },
+                    {
+                        img: '../../img/buy-dc-power-visa.svg',
+                        text: 'DC Power Visa'
+                    }
+                ]
+             }
+         }
+ };
+</script>
+
+<template>
+    <div class="bluebar">
+        <div class="innerbox">
+            <div class="box" v-for="(item, i) in blueImgs" :key="i">
+                <img :src="item.img" alt="">
+                <h4>{{item.text}}</h4>
+            </div>
+        </div>
+    </div>  
+</template>
+
+<style scoped>
+
+.bluebar{
+    width: 100%;
+    height: 150px;
+    background-color: rgb(8, 103, 197);
+    display: flex;
+    justify-content: center;
+}
+.innerbox{
+    widows: 80%;
+    display: flex;
+    justify-content: space-between;
+}
+.box{
+    display: flex;
+    align-items: center;
+    color: white;
+}
+
+.box img{
+    width: 50%;
+}
+
+</style>
